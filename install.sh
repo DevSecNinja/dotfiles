a75b9874f42f714b8662088010c2f602d4e560e9
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-05-20 14:19:30 +0000

    Add function to support codespaces dir

--- install.sh
+++ install.sh
@@ -61,6 +61,40 @@ function setup_go() {
   fi
 }
 
+function download_dotfiles () {
+  SOURCE="https://github.com/DevSecNinja/dotfiles"
+  CODESPACES_DOTFILES_PATH="/workspaces/.codespaces/.persistedshare/dotfiles"
+  TARBALL="$SOURCE/tarball/main"
+  TARGET="$HOME/.dotfiles"
+  TAR_CMD="tar -xzv -C "$TARGET" --strip-components=1 --exclude='{.gitignore}' --exclude='{LICENSE}' --exclude='{README.md}'"
+
+  is_executable() {
+    type "$1" > /dev/null 2>&1
+  }
+
+  if [ -d "$CODESPACES_DOTFILES_PATH" ]; then
+    echo "Found dotfiles location. Running in Codespaces..."
+    mkdir -p "$TARGET"
+    cp -nrf $CODESPACES_DOTFILES_PATH $TARGET
+    return;
+
+  if is_executable "git"; then
+    CMD="git clone $SOURCE $TARGET"
+  elif is_executable "curl"; then
+    CMD="curl -#L $TARBALL | $TAR_CMD"
+  elif is_executable "wget"; then
+    CMD="wget --no-check-certificate -O - $TARBALL | $TAR_CMD"
+  fi
+
+  if [ -z "$CMD" ]; then
+    echo "No git, curl or wget available. Aborting."
+  else
+    echo "Installing dotfiles..."
+    mkdir -p "$TARGET"
+    eval "$CMD"
+  fi
+}
+
 function move_dotfiles () {
   # Resolve DOTFILES_DIR (assuming ~/.dotfiles on distros without readlink and/or $BASH_SOURCE/$0)
 
