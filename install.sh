e51fce9514687faa6efe1cfe8069c17a2b8e4e61
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-05-20 18:48:27 +0000

    Prevent creating main folder with T switch

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ function download_dotfiles () {
   if [ -d "$CODESPACES_DOTFILES_PATH" ]; then
     echo "Found dotfiles location. Running in Codespaces..."
     mkdir -p "$TARGET"
-    cp -nrf $CODESPACES_DOTFILES_PATH $TARGET
+    cp -nrfT $CODESPACES_DOTFILES_PATH $TARGET
   else
     if is_executable "git"; then
       CMD="git clone $SOURCE $TARGET"
