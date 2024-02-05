b1292671242169ad6f862bf944f71b43ffd5c47e
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-05 20:16:51 +0100

    Fix homebrew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,19 +10,11 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
+  if $(command -v ruby) == ""
   then
-    brewRepo="Homebrew"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    brewRepo="Linuxbrew"
-  fi
-
-  if [[ $(command -v ruby) == "" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/$brewRepo/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
-    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/$brewRepo/install/HEAD/install.sh)"
+    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
 fi
