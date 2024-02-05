8c6a3b4876651ea22e7e0ee3a75b4998e95c077c
DevSecNinja <14926452+DevSecNinja@users.noreply.github.com>
2024-02-05 18:47:12 +0000

    Reorganize

--- system/env.zsh
+++ system/env.zsh
@@ -4,10 +4,3 @@ then
 else
   export EDITOR='nano'
 fi
-
-# History
-
-export HISTSIZE=32768;
-export HISTFILESIZE="${HISTSIZE}";
-export SAVEHIST=4096
-export HISTCONTROL=ignoredups:erasedups
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,6 +8,8 @@ autoload -U $ZSH/functions/*(:t)
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+HISTCONTROL=ignoredups:erasedups
+HISTFILESIZE="${HISTSIZE}";
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
