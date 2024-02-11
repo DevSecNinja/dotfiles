ad428079a4298bd8a2971a5d66933f0c3b318372
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-11 22:22:01 +0100

    Fix ohmyposh check

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 # Install Oh My Zsh
-if test $(which omz);
+if ! test $(which omz);
 then
   echo "Installing Oh My ZSH"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)" "" --unattended --keep-zshrc
