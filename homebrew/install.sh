75f3f5240951d32b5565619ebf219b24b0ccee7c
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-11 21:34:47 +0100

    Add sudo check

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,19 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+# Check if we can sudo
+# https://unix.stackexchange.com/a/692109
+sudo_response=$(SUDO_ASKPASS=/bin/false sudo -A whoami 2>&1 | wc -l)
+if [ $sudo_response = 2 ];
+then
+    can_sudo=1
+elif [ $sudo_response = 1 ];
+then
+    can_sudo=0
+else
+    echo "Unexpected sudo response: $sudo_response" >&2
+fi
+
 # Check for Homebrew & if not running interactively
 if test ! $(which brew) && [ ! -o interactive ]
 then
@@ -12,7 +25,11 @@ then
 
   if ! command -v ruby &> /dev/null
   then
-    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ $can_sudo = 0 ]; then
+      CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
   else
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
