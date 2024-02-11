e88b1dfdfb26c697c5d78b0ceede751f4d9349a8
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-11 21:45:28 +0000

    Add verbose log

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,4 +4,6 @@ if ! command -v omz &> /dev/null
 then
   echo "Installing Oh My ZSH"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)" "" --unattended --keep-zshrc
+else
+  echo "Oh My ZSH already installed"
 fi
