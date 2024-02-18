938dc59947e543b05bd966759378f8ba951486de
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-18 12:32:28 +0100

    Don't run brew install when in background job

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,8 +2,8 @@
 # Install Oh My Zsh
 if ! test omz # command -v doesn't seem to work for some reason via script in zsh
 then
-  echo "Installing Oh My ZSH"
+  echo "installing Oh My ZSH"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)" "" --unattended --keep-zshrc
 else
-  echo "Oh My ZSH already installed"
+  echo "oh my zsh already installed"
 fi
