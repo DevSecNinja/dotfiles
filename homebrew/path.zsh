984cfdabbaf9892d687a69b0e29a143e722d0264
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-02-18 10:27:45 +0100

    Load functions and others on zsh session start

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,8 +1,14 @@
 # Install the correct homebrew for each OS type
 if test "$(uname)" = "Darwin"
 then
-  export PATH="/opt/homebrew/bin:$PATH"
+  if ! echo "$PATH" | grep -q "/opt/homebrew/bin"
+  then
+    export PATH="/opt/homebrew/bin:$PATH"
+  fi
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
-  export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  if ! echo "$PATH" | grep -q "/home/linuxbrew/.linuxbrew/bin"
+  then
+    export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  fi
 fi
