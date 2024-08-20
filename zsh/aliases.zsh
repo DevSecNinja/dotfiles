7b1e76bc34191d1bf0f050ba8d1219eeef74bfa7
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-08-20 12:39:57 +0200

    Add new alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+# For the occasions sudo isn't sufficient, this loads
+# existing ZSH config into root shell so that I can run a function like resize-disk
+alias root-and-load='sudo -E su -p'
