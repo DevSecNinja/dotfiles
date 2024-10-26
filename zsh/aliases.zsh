5fbcab48b02f81ce108ee26e72c219b8debd37d1
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-10-26 15:52:05 +0200

    Add get-kubedash-token alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
 # For the occasions sudo isn't sufficient, this loads
 # existing ZSH config into root shell so that I can run a function like resize-disk
 alias root-and-load='sudo -E su -p'
+
+# Gets the token for the Kubernetes Dashboard
+alias get-kubedash-token='kubectl -n observability get secret kubernetes-dashboard -o jsonpath='{.data.token}' | base64 -d'
