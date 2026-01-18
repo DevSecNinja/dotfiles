4ba2076fff1dc5e685cab1dbf14daf17f9bce5ce
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2026-01-18 23:36:09 +0100

    Initialize brew

--- home/dot_config/shell/config.bash
+++ home/dot_config/shell/config.bash
@@ -2,6 +2,15 @@
 # Bash configuration
 # This file should be sourced by ~/.bashrc or ~/.bash_profile
 
+# Initialize Homebrew (macOS/Linux)
+if [ -f "/opt/homebrew/bin/brew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -f "/usr/local/bin/brew" ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+elif [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 # Load all functions from shell/functions directory
 if [ -d "$HOME/.config/shell/functions" ]; then
 	for func_file in "$HOME/.config/shell/functions"/*; do
